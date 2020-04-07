d8efde5733d017cfb328b977f2bc4ec5988ce166
jeremy <jeremy.langlet@gmail.com>
2020-04-07 09:53:17 +0200

    ajout/suppression d'une annonce

---
+++ client/src/components/pages/AddAdPage.js
@@ -0,0 +1,97 @@
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
+
+import Header from '../Header'
+
+class AddAdPage extends Component {
+    state = {
+        title: '',
+        content: '',
+        postcode: ''
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target
+        this.setState({ [name]: value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        //on recupère le token
+        const token = window.localStorage.getItem("authToken")
+        //on le met dans un header
+        const config = {
+            headers: { Authorization: `Bearer ${token}` }
+        };
+        const ad = {
+            title: this.state.title,
+            content: this.state.content,
+            postcode: this.state.postcode
+        };
+        //on donne le header et les données à axios
+        axios.post( 
+            'http://localhost:8000/api/ads',
+            ad,
+            config
+          );
+
+            this.setState({
+                title: '',
+                content: '',
+                postcode: ''
+            })
+            
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Header/>
+                <div className="Content">
+                    <form
+                        className='form'
+                        onSubmit= {this.handleSubmit}>
+                        <label>
+                            <h3>Titre de l'annonce</h3>
+
+                            <input
+                                name='title'
+                                value={this.state.title}
+                                onChange={this.handleChange}
+                                type="text"
+                                required >
+                            </input>
+
+                            <h3>Description de l'annonce</h3>
+
+                            <textarea
+                                name='content'
+                                value={this.state.content}
+                                onChange={this.handleChange}
+                                type="text"
+                                rows="10" 
+                                cols="40"
+                                required
+                            />
+
+                            <h3>Localisation</h3>
+
+                            <input
+                                name='postcode'
+                                value={this.state.postcode}
+                                onChange={this.handleChange}
+                                type="text"
+                                required >
+                            </input>
+                        </label>
+                        <button className="btn" type='submit' >
+                            Envoyer!
+                        </button>
+                    </form>
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+export default AddAdPage;
