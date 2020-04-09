44c10d52ce702fe3ced06f24cfc549699fa58f6c
Adrien <adevweb00@gmail.com>
2020-04-09 16:33:06 +0200

    Ajout de la page MySpacePage et de ses composants

--- client/src/components/MyRecipes.js
+++ client/src/components/MyRecipes.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import Recipe from './Recipe'
+import DisplayMyRecipe from './DisplayMyRecipe'
 
-const MyRecipes = ({ recipes }) => {
-	const myRecipes = recipes.filter(fav => fav.isFav === false)
-		.map((recipe, i) => {
-			return <Recipe key={i} title={recipe.title} isFav={recipe.isFav} />
-		})
+const MyRecipes = ({ Recipes, handleDeleteRecipe }) => {
+	
+	function showSectionMyRecipe() {
+		document.querySelector(".SectionMyRecipe").classList.toggle("hideSection")
+	}
+
 	return (
-		<div className="Section">
-			<h2 className="SectionTitle">
+		<div className="SectionMyRecipe">
+			<h2 className="SectionTitle" onClick={showSectionMyRecipe}>
 				<svg className="iconArrow" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path d="M11.5 5L7 9L2.5 5" stroke="#444444" strokeWidth="1.5" strokeLinecap="round"/>
 				</svg>
 				Mes recettes
 			</h2>
 			<div className="SectionContent">
-				{myRecipes}
+				<DisplayMyRecipe Recipes={Recipes} handleDeleteRecipe={handleDeleteRecipe} />
 			</div>
 		</div>
 	)
