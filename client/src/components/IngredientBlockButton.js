42609db4008ecc8581ded2f71514c9b27e2f14ac
jeremy <jeremy.langlet@gmail.com>
2020-05-08 11:04:21 +0200

    correction icone trouver une recette

--- client/src/components/IngredientBlockButton.js
+++ client/src/components/IngredientBlockButton.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-const IngredientBlockButton = ({name, handleAdd}) => {
+const IngredientBlockButton = ({name, handleAdd, value, icon}) => {
 
     // Ajoute l'icone correspondante au nom de l'ingredient selectionné
     const requireIcon = (name) => {
         try {
-            return require(`../icons/ingredients/${name}.svg`)
+            return require(`../icons/ingredients/${icon}`)
         } catch (err) {
             return require(`../icons/ingredients/defaut-boissons.svg`)
         }
     }
     const test = {
-        backgroundImage: `url(${requireIcon(name)})`,
+        backgroundImage: `url(${requireIcon(icon)})`,
         backgroundRepeat: "no-repeat"
       };
     return (
-        <button className="ingredientBlock" onClick={handleAdd} value={name} style={test}>
+        <button className="ingredientBlock" onClick={handleAdd} value={value} style={test}>
             {name}
         </button>
     );
